c2e230cce5f14cd644c02b931408b7ed782fb7f3
Peter Torres <torres7p@gmail.com>
2021-07-28 01:01:21 -0700

    added detail route

---
+++ data.js
@@ -0,0 +1,19 @@
+let games = [
+    { name : "assasin'screed", console : "playstation", genre: "fantasy", hours : 99 },
+    { name : "supermario", console : "nintendo", genre: "kids", hours : 40 },
+    { name : "halo", console : "xbox", genre: "shooting", hours : 10 },
+    { name : "dragonball", console : "playstation", genre: "fighting", hours : 20 },
+    { name : "callofduty", console : "xbox", genre: "rpg", hours : 50 }
+];
+
+const getAll = () => {
+    return games;
+}
+
+const getItem = (name) => {
+    return games.find((game) => {
+        return game.name === name;
+    });
+};
+
+export { getAll, getItem };
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const http = require("http");
+import http from 'http';
+import * as info from './data.js';
+import { parse } from "querystring";
 
 // starts an HTTP server
 http.createServer((req,res) => {
-    var path = req.url.toLowerCase();
+    let url = req.url.split("?");
+    let query = parse(url[1]);
+    var path = url[0].toLowerCase();
+
     switch(path) {
         case '/':
             res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Home page');
+            res.end(JSON.stringify(info.getAll()));
             break;
         case '/about':
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end('I like anime');
             break;
+        case '/detail':
+            let game = info.getItem(query.name);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            let results = (game) ? JSON.stringify(game) : "Not found";
+            res.end('Your game is ' + query.name + "\n" + results);
+            break;
         default:
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end('Not found');
-            break;
     }
 }).listen(process.env.PORT || 3000);
