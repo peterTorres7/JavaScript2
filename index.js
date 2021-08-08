703c859a31830ee9c3a41248f519c8ba61217af3
Peter Torres <torres7p@gmail.com>
2021-08-08 16:01:35 -0700

    removed previously commented out http use

--- index.js
+++ index.js
@@ -48,30 +48,3 @@ app.use((req,res) => {
 app.listen(app.get('port'), () => {
     console.log('Express started');
 });
-
-// // starts an HTTP server
-// http.createServer((req,res) => {
-//     let url = req.url.split("?");
-//     let query = parse(url[1]);
-//     var path = url[0].toLowerCase();
-
-//     switch(path) {
-//         case '/':
-//             res.writeHead(200, {'Content-Type': 'text/plain'});
-//             res.end(JSON.stringify(data.getAll()));
-//             break;
-//         case '/about':
-//             res.writeHead(200, {'Content-Type': 'text/plain'});
-//             res.end('I like anime');
-//             break;
-//         case '/detail':
-//             let game = data.getItem(query.name);
-//             res.writeHead(200, {'Content-Type': 'text/plain'});
-//             let results = (game) ? JSON.stringify(game) : "Not found";
-//             res.end('Your game is ' + query.name + "\n" + results);
-//             break;
-//         default:
-//             res.writeHead(404, {'Content-Type': 'text/plain'});
-//             res.end('Not found');
-//     }
-// }).listen(process.env.PORT || 3000);
