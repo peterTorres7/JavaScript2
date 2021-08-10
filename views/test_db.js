5dd30f7585ff3d9dd443ed2a4aa7f59f49bb9012
Peter Torres <torres7p@gmail.com>
2021-08-10 11:02:02 -0700

    added database test file

--- views/test_db.js
+++ views/test_db.js
@@ -3,19 +3,40 @@
 import { Game } from "../models/game.js";
 
 //find all documents
-Game.find({}, (err, result) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(result);
-    }
-    return
-});;
+// Game.find({}, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(result);
+//     }
+//     return
+// });;
+
+Game.find({}).lean()
+    .then((games) => {
+        console.log(games);
+    })
+    .catch(err => next(err));
+
+// return all matching records
+// Game.find({"console": "playstation"}).lean()
+//     .then((games) => {
+//         console.log(game);
+//     })
+//     .catch(err => next(err));
+
+// // return single record
+// Game.findOne({"name": "halo"}).lean()
+//     .then((games) => {
+//         console.log(game);
+//     })
+//     .catch(err => next(err));
+
 
 // count number of docs
 console.log("step 1")
-Book.countDocuments((err, result) => {
-    console.log("stemp 2")
+Game.countDocuments((err, result) => {
+    console.log("step 2")
     console.log(result + " db entries");
 });
 console.log("step 3")
